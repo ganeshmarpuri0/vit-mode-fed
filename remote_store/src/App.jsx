c4e1be0070085723e1b369fd7e2af370e3c98eb8
ganeshmarpuri0 <ganesh.marpuri@sysco.com>
2025-11-14 08:23:41 -0600

    feat: integrate state management with useStore for count and increment functionality

--- remote_store/src/App.jsx
+++ remote_store/src/App.jsx
@@ -2,13 +2,18 @@
 import './App.css'
 import Button1 from 'Button1/Button1'
 import Button2 from 'Button2/Button2'
+import useStore from './statemangement/store.js'
 function App() {
   // const [count, setCount] = useState(0)
+  const { count, increment,  } = useStore();
+
 
   return (
     <>
       <Button1 />
       <Button2 />
+      <button onClick={increment}>Increment</button>
+      <p>Count: {count}</p>
 
       <h1>Store</h1>
 
